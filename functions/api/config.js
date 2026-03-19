1c07c057945c302a1e01949057310ab088394bb0
focoemcanto-pixel <focoemcanto@gmail.com>
2026-03-18 21:14:03 -0300

    Add onRequestGet function to fetch API config

--- functions/api/config.js
+++ functions/api/config.js
@@ -1,1 +1,21 @@
+export async function onRequestGet() {
+  const apiUrl = 'https://script.google.com/macros/s/AKfycbxp1xY_Pqsde9Jrs9hbwVr2vTZuqQt2DU7-c5uQQJ0lmBUalNmgIIrXN1XJHNkILpJv/exec?route=config';
 
+  const res = await fetch(apiUrl, {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json'
+    }
+  });
+
+  const text = await res.text();
+
+  return new Response(text, {
+    status: res.status,
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+      'Cache-Control': 'no-store',
+      'Access-Control-Allow-Origin': '*'
+    }
+  });
+}
