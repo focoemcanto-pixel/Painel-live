9a02b5c0574e75394b4e957f7af2bf591b58846d
focoemcanto-pixel <focoemcanto@gmail.com>
2026-03-18 21:15:44 -0300

    Add POST and OPTIONS request handlers

--- functions/api/action.js
+++ functions/api/action.js
@@ -1,1 +1,36 @@
 
+export async function onRequestPost(context) {
+  const body = await context.request.text();
+
+  const apiUrl = 'https://script.google.com/macros/s/AKfycbxp1xY_Pqsde9Jrs9hbwVr2vTZuqQt2DU7-c5uQQJ0lmBUalNmgIIrXN1XJHNkILpJv/exec';
+
+  const res = await fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body
+  });
+
+  const text = await res.text();
+
+  return new Response(text, {
+    status: res.status,
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+      'Cache-Control': 'no-store',
+      'Access-Control-Allow-Origin': '*'
+    }
+  });
+}
+
+export async function onRequestOptions() {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'POST, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type'
+    }
+  });
+}
