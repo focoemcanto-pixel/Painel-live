c316a6736ce41dc892a0fc44415f9811b3c7547e
focoemcanto-pixel <focoemcanto@gmail.com>
2026-03-18 21:30:32 -0300

    Update hello.js

--- functions/hello.js
+++ functions/hello.js
@@ -1,1 +1,7 @@
-
+export async function onRequestGet() {
+  return new Response(JSON.stringify({ ok: true, hello: "world" }), {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+}
